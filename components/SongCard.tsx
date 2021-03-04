de8e0e74050f2d336ad14413eb5f3090be87042d
Avishek Mondal <avishek.mondal53@gmail.com>
2021-03-04 15:46:28 +0530

    refactored css, changed layout

--- components/SongCard.tsx
+++ components/SongCard.tsx
@@ -1,8 +1,62 @@
+// import { Typography } from "@material-ui/core";
 import React from "react";
-import styles from "../styles/Home.module.css";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import { Typography, Container } from "@material-ui/core";
 
+import styles from "../styles/SongCard.module.css";
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 const SongCard: React.FC = () => {
-  return <div className={styles.songCard}></div>;
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
+  return (
+    // <div className={styles.songCard}>
+    <Card className={(classes.root, styles.songCard)}>
+      <CardContent>
+        <Typography
+          className={classes.title}
+          color="textSecondary"
+          gutterBottom
+        >
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="h2">
+          be{bull}nev{bull}o{bull}lent
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          adjective
+        </Typography>
+        <Typography variant="body2" component="p">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
 };
 
 export default SongCard;
